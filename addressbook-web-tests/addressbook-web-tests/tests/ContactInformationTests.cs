442c73ed81ec159a17e25d9615c2ef63bd792ab5
Alena Umreika <umreiko.elena@gmail.com>
2021-06-28 00:16:15 +0300

    Checking Contact Info on Details Info page (Task 12)

--- addressbook-web-tests/addressbook-web-tests/tests/ContactInformationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactInformationTests.cs
@@ -21,7 +21,17 @@ namespace WebAddressbookTests
             Assert.AreEqual(fromTable.Address, fromForm.Address);
             Assert.AreEqual(fromTable.AllEmails, fromForm.AllEmails);
             Assert.AreEqual(fromTable.AllPhones, fromForm.AllPhones);
-            
+
+        }
+
+        [Test]
+        public void TestDetailedContactInformation()
+        {
+            string fromDetailedForm = app.Contacts.GetContactInformationFromDetailedForm(0);
+            ContactData fromForm = app.Contacts.GetContactInformationFromForm(0);
+
+            //verification
+            Assert.AreEqual(fromDetailedForm, fromForm.AllDetailedInfo);
 
         }
     }
