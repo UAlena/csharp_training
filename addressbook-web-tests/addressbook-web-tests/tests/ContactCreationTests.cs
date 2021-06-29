b480d80640e6cce4c05fc559a51cdf0ba7977ad1
Alena Umreika <umreiko.elena@gmail.com>
2021-06-29 23:09:02 +0300

    Test Parameterization (Task 13)

--- addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -11,13 +11,24 @@ namespace WebAddressbookTests
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        [Test]
-        public void ContactCreationTest()
+        public static IEnumerable<ContactData> RandomContactDataProvider()
         {
-            ContactData contact = new ContactData("first");
-            contact.Middlename = "middle";
-            contact.Lastname = "last";
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(30))
+                {
+                    Middlename = GenerateRandomString(100),
+                    Lastname = GenerateRandomString(100)
+                });
+            }
+            return contacts;
+        }
 
+        [Test, TestCaseSource("RandomContactDataProvider")]
+        public void ContactCreationTest(ContactData contact)
+        {
+            
             List<ContactData> oldContacts = app.Contacts.GetContactList();
 
             app.Contacts.Create(contact);
